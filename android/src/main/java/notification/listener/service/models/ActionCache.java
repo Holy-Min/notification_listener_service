928dc516724f35d6db9d570f3b857e4f34c4d136
Holy-Min <97871977+Holy-Min@users.noreply.github.com>
2022-08-27 04:35:39 +0900

    Update ActionCache.java

--- android/src/main/java/notification/listener/service/models/ActionCache.java
+++ android/src/main/java/notification/listener/service/models/ActionCache.java
@@ -3,5 +3,6 @@ package notification.listener.service.models;
 import java.util.HashMap;
 
 abstract public class ActionCache {
-    public static HashMap<Integer, Action> cachedNotifications = new HashMap<>();
+//    public static HashMap<Integer, Action> cachedNotifications = new HashMap<>();
+    public static HashMap<String, Action> cachedNotifications = new HashMap<>();
 }
