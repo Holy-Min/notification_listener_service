47feee9be514e545491ec65ab927d4ff7a149aab
Holy-Min <97871977+Holy-Min@users.noreply.github.com>
2022-08-27 03:28:13 +0900

    Update NotificationListener.java

--- android/src/main/java/notification/listener/service/NotificationListener.java
+++ android/src/main/java/notification/listener/service/NotificationListener.java
@@ -50,7 +50,7 @@ public class NotificationListener extends NotificationListenerService {
 
         NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         StatusBarNotification[] barNotifications = notificationManager.getActiveNotifications();
-        System.out.println("리스트 길이" + barNotifications.length);
+//         System.out.println("리스트 길이" + barNotifications.length);
         System.out.println("노티피케이션 확인 :" + barNotifications);  
         for(int i = 0; i < barNotifications.length; i++) {
             System.out.println("노티피케이션 확인 :" + barNotifications[i]);    
