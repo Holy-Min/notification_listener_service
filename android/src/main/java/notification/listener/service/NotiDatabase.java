5a6c063bfe3e7f6d5d4c9bf53923c3675eabebd6
Holy-Min <97871977+Holy-Min@users.noreply.github.com>
2022-09-24 17:58:45 +0900

    Update NotiDatabase.java

--- android/src/main/java/notification/listener/service/NotiDatabase.java
+++ android/src/main/java/notification/listener/service/NotiDatabase.java
@@ -25,7 +25,7 @@ public abstract class NotiDatabase extends RoomDatabase {
     public static synchronized NotiDatabase getInstance(Context context){
         if (database == null)
         {
-            database = Room.databaseBuilder(context.getApplicationContext(), NotiDatabase.class, "messageTest.db")
+            database = Room.databaseBuilder(context.getApplicationContext(), NotiDatabase.class, "messageVer1.db")
 //                     .addMigrations(MIGRATION_1_2)
                     .allowMainThreadQueries()
                     .build();
