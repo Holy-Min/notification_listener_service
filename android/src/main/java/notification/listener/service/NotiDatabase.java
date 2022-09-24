8fe3c333ab8447cd14cc9599f1966de9fe58eada
Holy-Min <97871977+Holy-Min@users.noreply.github.com>
2022-09-24 17:56:31 +0900

    Update NotiDatabase.java

--- android/src/main/java/notification/listener/service/NotiDatabase.java
+++ android/src/main/java/notification/listener/service/NotiDatabase.java
@@ -6,12 +6,12 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 import androidx.room.TypeConverters;
-import androidx.room.Migration;
+// import androidx.room.Migration;
 import androidx.sqlite.db.SupportSQLiteDatabase;
 
 
 @Database(entities = {NotiData.class, KakaoData.class, WhatsappData.class, RoomData.class, KakaoRoomData.class, WhatsappRoomData.class},
-        version = 2, exportSchema = false)
+        version = 1, exportSchema = false)
 public abstract class NotiDatabase extends RoomDatabase {
     public abstract NotiDataDao NotiDao();
     public abstract KakaoDataDao KakaoDao();
@@ -25,8 +25,8 @@ public abstract class NotiDatabase extends RoomDatabase {
     public static synchronized NotiDatabase getInstance(Context context){
         if (database == null)
         {
-            database = Room.databaseBuilder(context.getApplicationContext(), NotiDatabase.class, "messageTest2.db")
-                    .addMigrations(MIGRATION_1_2)
+            database = Room.databaseBuilder(context.getApplicationContext(), NotiDatabase.class, "messageTest.db")
+//                     .addMigrations(MIGRATION_1_2)
                     .allowMainThreadQueries()
                     .build();
         }
@@ -34,14 +34,14 @@ public abstract class NotiDatabase extends RoomDatabase {
 
     }
 
-    static final Migration MIGRATION_1_2 = new Migration(1, 2) {
-        @Override
-        public void migrate(SupportSQLiteDatabase database) {
-            database.execSQL("ALTER TABLE KakaoData" + "ADD COLUMN 'read' INTEGER DEFAULT 2");
-            database.execSQL("ALTER TABLE NotiData" + "ADD COLUMN 'read' INTEGER DEFAULT 2");
-            database.execSQL("ALTER TABLE WhatsappData" + "ADD COLUMN 'read' INTEGER DEFAULT 2");
-        }
-    };
+//     static final Migration MIGRATION_1_2 = new Migration(1, 2) {
+//         @Override
+//         public void migrate(SupportSQLiteDatabase database) {
+//             database.execSQL("ALTER TABLE KakaoData" + "ADD COLUMN 'read' INTEGER DEFAULT 2");
+//             database.execSQL("ALTER TABLE NotiData" + "ADD COLUMN 'read' INTEGER DEFAULT 2");
+//             database.execSQL("ALTER TABLE WhatsappData" + "ADD COLUMN 'read' INTEGER DEFAULT 2");
+//         }
+//     };
 
     public static void destroyInstance() {
         database = null;
