68989d9a5f22d2ee0bf07100d902d731544eb324
Holy-Min <97871977+Holy-Min@users.noreply.github.com>
2022-09-24 17:53:53 +0900

    Update NotiDatabase.java

--- android/src/main/java/notification/listener/service/NotiDatabase.java
+++ android/src/main/java/notification/listener/service/NotiDatabase.java
@@ -6,7 +6,7 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 import androidx.room.TypeConverters;
-//import androidx.room.Migration
+import androidx.room.Migration
 import androidx.sqlite.db.SupportSQLiteDatabase;
 
 
