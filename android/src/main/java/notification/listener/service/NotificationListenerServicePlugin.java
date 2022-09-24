e47ef35abc998d198c6d3cf4e3a2ff8fe0b720aa
Holy-Min <97871977+Holy-Min@users.noreply.github.com>
2022-09-24 18:46:37 +0900

    Update NotificationListenerServicePlugin.java

--- android/src/main/java/notification/listener/service/NotificationListenerServicePlugin.java
+++ android/src/main/java/notification/listener/service/NotificationListenerServicePlugin.java
@@ -321,7 +321,7 @@ public class NotificationListenerServicePlugin implements FlutterPlugin, Activit
                 noti.send = 1;
                 noti.result = str;
                 noti.app = 1;
-                noti.read = 2;
+                noti.read = "2";
 
                 notiDb.NotiDao().insert(noti);
             } else if(packageName.equals("com.kakao.talk")) {
@@ -333,7 +333,7 @@ public class NotificationListenerServicePlugin implements FlutterPlugin, Activit
                 kakaonoti.send = 1;
                 kakaonoti.result = str;
                 kakaonoti.app = 2;
-                kakaonoti.read = 2;
+                kakaonoti.read = "2";
 
                 notiDb.KakaoDao().insert(kakaonoti);
             } else if(packageName.equals("com.whatsapp")) {
@@ -345,7 +345,7 @@ public class NotificationListenerServicePlugin implements FlutterPlugin, Activit
                 whatsappnoti.send = 1;
                 whatsappnoti.result = str;
                 whatsappnoti.app = 3;
-                whatsappnoti.read = 2;
+                whatsappnoti.read = "2";
 
                 notiDb.WhatsappDao().insert(whatsappnoti);
             }
