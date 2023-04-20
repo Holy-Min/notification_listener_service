1f8cf0fd01015c8342462e0878c56be01722e08f
Holy-Min <97871977+Holy-Min@users.noreply.github.com>
2023-04-20 14:20:16 +0900

    Update WhatsappDataDao.java

--- android/src/main/java/notification/listener/service/WhatsappDataDao.java
+++ android/src/main/java/notification/listener/service/WhatsappDataDao.java
@@ -58,7 +58,7 @@ public interface WhatsappDataDao {
     @Query("select count(*) from WhatsappData")
     int total();
 
-    @Query("select count(*) from WhatsappData where result NOT in ('101', '111', '201', '211', '301', '311', 'yet')")
+    @Query("select count(*) from WhatsappData where result NOT in ('101', '111', '201', '211', '301', '311', 'yet', 'N')")
     int resultCount();
 
     @Query("select text from WhatsappData where room = :room order by nid desc limit 1")
