69ef042db5eb43e2a380b1f11da1997df411fd9a
Holy-Min <97871977+Holy-Min@users.noreply.github.com>
2023-04-21 17:47:54 +0900

    Update WhatsappDataDao.java

--- android/src/main/java/notification/listener/service/WhatsappDataDao.java
+++ android/src/main/java/notification/listener/service/WhatsappDataDao.java
@@ -28,8 +28,8 @@ public interface WhatsappDataDao {
 //    @Query("update WhatsappData set result = :result, url = :url where nid = (select nid from (select * from WhatsappData where url = 'yet' and result = 'yet' limit 1))")
 //    void update(String result, String url);
 
-    @Query("update WhatsappData set result = :result, url = :url where nid = :nid")
-    void update(String result, String url, int nid);
+    @Query("update WhatsappData set result = :result, url = :url, text = :text where nid = :nid")
+    void update(String result, String url, int nid, String text);
 
 //    @Query("update WhatsappData set result = :result where nid = (select nid from (select * from WhatsappData where result = 'yet' limit 1))")
 //    void update(String result);
