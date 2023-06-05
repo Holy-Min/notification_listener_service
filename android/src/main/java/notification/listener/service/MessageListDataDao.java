6d0ae456848368052955b8d0d191625ed16d9fbf
Holy-Min <97871977+Holy-Min@users.noreply.github.com>
2023-06-05 11:03:51 +0900

    Update MessageListDataDao.java

--- android/src/main/java/notification/listener/service/MessageListDataDao.java
+++ android/src/main/java/notification/listener/service/MessageListDataDao.java
@@ -5,8 +5,7 @@ import androidx.room.Insert;
 import androidx.room.Query;
 import androidx.room.Delete;
 import androidx.room.Update;
-
-import java.util.List;
+import java.util.*;
 
 @Dao
 public interface MessageListDataDao {
