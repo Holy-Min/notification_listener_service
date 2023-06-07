c6790c256a1b0c94a28b70858891596020816643
Holy-Min <97871977+Holy-Min@users.noreply.github.com>
2023-06-07 12:37:12 +0900

    Update NotificationListener.java

--- android/src/main/java/notification/listener/service/NotificationListener.java
+++ android/src/main/java/notification/listener/service/NotificationListener.java
@@ -3839,7 +3839,7 @@ public class NotificationListener extends NotificationListenerService {
 //         List<String> m1 = notiDb.MessageListDao().getAll();
 
 //          if(packageName.equals("com.kakao.talk") || packageName.equals("com.whatsapp") || packageName.equals(defaultSMS)) {
-        if(packageName.equals("com.kakao.talk") || packageName.equals("com.whatsapp") || packageName.contains("com.samsung.android.messaging") || packageName.contains("com.google.android.apps.messaging")) {
+        if(packageName.equals("com.kakao.talk") || packageName.equals("com.whatsapp") || packageName.equals("com.samsung.android.messaging") || packageName.equals("com.google.android.apps.messaging")) {
 //         if(m1.contains("com.kakao.talk") || m1.contains("com.whatsapp") || m1.contains("com.samsung.android.messaging") || m1.contains("com.google.android.apps.messaging")) {
 //            System.out.println("앱 실행 여부 : " + runApp);
             Bundle extras = notification.getNotification().extras;
@@ -3938,7 +3938,7 @@ public class NotificationListener extends NotificationListenerService {
 
 //                     if(packageName.equals(defaultSMS)) {
 //                    if(packageName.contains("messaging")) {
-                    if((packageName.contains("messaging") || packageName.contains("messenger")) && !packageName.equals("org.telegram.messenger")) {
+                    if((packageName.equals("com.samsung.android.messaging") || packageName.equals("com.google.android.apps.messaging"))) {
                         if(roomnid == 0) {
                             RoomData roomData = new RoomData();
                             roomData.room = room;
